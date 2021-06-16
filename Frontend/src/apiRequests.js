0deac1069386db988f765ce79c0bb212d4588ee4
Faris Ibrahim <procusr@gmail.com>
2021-06-16 19:26:11 +0200

    Test added

--- Frontend/src/apiRequests.js
+++ Frontend/src/apiRequests.js
@@ -1,14 +1,15 @@
 import axios from "axios";
 
+
 const baseUrl = "http://localhost:4000/users";
 
 const getAllStatus = () => {
-  return axios.get(baseUrl).then((response) => response.data);
+   return axios.get(baseUrl).then((response) => response.data);
 };
 
 const updateStatus = (id, obj) => {
-    const request = axios.put(`${baseUrl}/${id}`, obj)
-     return request.then(response => response.data)
+      const request = axios.put(`${baseUrl}/${id}`, obj)
+        return request.then(response => response.data)
 }
 
 export default {
